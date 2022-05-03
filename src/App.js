a6b75d2ba719d4a6ee3b92f40e8a48b2751d7c4b
SAGAR J <75424678+sagarjgb@users.noreply.github.com>
2022-05-03 19:13:53 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PaginatedItems from './page';
+
+function App() {
+  return (
+    <div className="App">
+      <PaginatedItems itemsPerPage={1} />
+    </div>
+  );
+}
+
+export default App;
